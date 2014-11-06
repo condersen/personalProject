cf9264db9871760d44922508bdaab889a62e52d5
connor andersen <condersen@gmail.com>
2014-11-06 15:51:41 -0800

    fresh first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap-theme.min.css">
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+	<body>
+		<div id="layout-container">
+			<div id="header-container">
+				<div class="nav-container">
+					<div class="nav-bar">
+						<div class="logostuff"><a href="#"><h1>IMPORTANT THINGS</h1></a></div>
+						<ul class="list-clean nav-list">
+							<li class="nav-button">
+								<a href="#">Home</a>
+							</li>
+							<li class="nav-button">
+								<a href="#">About</a>
+							</li>
+							<li class="nav-button">
+								<a href="#">Contact</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div id="content-container">
+				<div class="cold-md-12">
+					
+				</div>
+				<div>
+					
+				</div>
+				<div>
+					
+				</div>
+			</div>
+			<div id="footer-container">
+				
+			</div>
+		</div>
+	</body>
+</html>
